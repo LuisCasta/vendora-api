0a26eb9d1472b9d2f6dedc732b7a5ffa760933f4
villalobos xbio <villalobos@x-biomedical.com>
2026-03-05 21:53:26 -0600

    Add nestjs and prisma setup

---
+++ src/modules/auth/auth.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { DatabaseModule } from '../../database/database.module';
+
+@Module({
+  imports: [
+    DatabaseModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '15m' },
+    }),
+  ],
+  providers: [AuthService],
+  controllers: [AuthController],
+})
+export class AuthModule {}
